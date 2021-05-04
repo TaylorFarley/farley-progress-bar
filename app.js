88e6b64db96c46502da7f183c1265c66a7443875
Taylor <twfarley88@gmail.com>
2021-05-03 21:33:48 -0400

    first

---
+++ app.js
@@ -0,0 +1,43 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+
+var app = express();
+
+// view engine setup
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+if(process.env.PORT){
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+  
+  
+  app.use((req, res, next) => {
+    next(createError(404));
+  });
+
+  app.use(function (err, req, res, next) {
+    console.error(err.message);
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+  });
+  }
+
+module.exports = app;
